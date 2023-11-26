8aa2ce2c9fc4ca629456869e3e8cdea92b5bb4dd
dflorez6 <dflorez6@gmail.com>
2023-11-26 00:32:09 -0500

    DB Seed

---
+++ assignment2/Migrations/20231126053041_DBSeed.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace assignment2.Migrations
+{
+    /// <inheritdoc />
+    public partial class DBSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "CourseId", "Instructor", "Name", "Room", "Start" },
+                values: new object[,]
+                {
+                    { 1, "Peter Mazdiak", "Programming Microsoft Web Technologies", "1C09", new DateTime(2024, 1, 18, 14, 30, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "Noman Atique", "Programming Concepts II", "4G15", new DateTime(2024, 1, 15, 12, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "Peter Mazdiak", "Distributed Application Development", "3G19", new DateTime(2023, 12, 19, 9, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "CourseId", "Email", "Name", "StatusId" },
+                values: new object[,]
+                {
+                    { 1, 1, "dflorez6@gmail.com", "Bart Simpson", "ConfirmationMessageNotSent" },
+                    { 2, 1, "dflorez6.dev@gmail.com", "Lisa Simpson", "ConfirmationMessageNotSent" },
+                    { 3, 2, "dflorez6@gmail.com", "Marge Simpson", "ConfirmationMessageNotSent" },
+                    { 4, 2, "dflorez6.dev@gmail.com", "Homer Simpson", "ConfirmationMessageNotSent" },
+                    { 5, 3, "dflorez6@gmail.com", "Maggie Simpson", "ConfirmationMessageNotSent" },
+                    { 6, 3, "dflorez6.dev@gmail.com", "Ned Flanders", "ConfirmationMessageNotSent" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 3);
+        }
+    }
+}
--- assignment2/Models/CoursesContext.cs
+++ assignment2/Models/CoursesContext.cs
@@ -36,12 +36,96 @@ namespace assignment2.Models
                 new Status { StatusId = "EnrollmentDeclined", StatusName = "Enrollment Declined" }
             );
 
-            // Courses
-
-            // Students
+            // Associations
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Students)
+                .WithOne(s => s.Course)
+                .HasForeignKey(s => s.CourseId)
+                .OnDelete(DeleteBehavior.Cascade); // Used to delete associated students records when deleting a course
 
             // Default value for StatusId in Student
             modelBuilder.Entity<Student>().Property(s => s.StatusId).HasDefaultValue("ConfirmationMessageNotSent");
+
+            // Courses
+            modelBuilder.Entity<Course>().HasData(
+                new Course
+                {
+                    CourseId = 1,
+                    Name = "Programming Microsoft Web Technologies",
+                    Instructor = "Peter Mazdiak",
+                    Start = new DateTime(2024, 01, 18, 14, 30, 0),
+                    Room = "1C09"
+                },
+                new Course
+                {
+                    CourseId = 2,
+                    Name = "Programming Concepts II",
+                    Instructor = "Noman Atique",
+                    Start = new DateTime(2024, 01, 15, 12, 0, 0),
+                    Room = "4G15"
+                },
+                new Course
+                {
+                    CourseId = 3,
+                    Name = "Distributed Application Development",
+                    Instructor = "Peter Mazdiak",
+                    Start = new DateTime(2023, 12, 19, 9, 0, 0),
+                    Room = "3G19"
+                }
+            );
+
+            // Students
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    StudentId = 1,
+                    Name = "Bart Simpson",
+                    Email = "dflorez6@gmail.com",
+                    CourseId = 1,
+                    StatusId = "ConfirmationMessageNotSent"
+                },
+                new Student
+                {
+                    StudentId = 2,
+                    Name = "Lisa Simpson",
+                    Email = "dflorez6.dev@gmail.com",
+                    CourseId = 1,
+                    StatusId = "ConfirmationMessageNotSent"
+                },
+                new Student
+                {
+                    StudentId = 3,
+                    Name = "Marge Simpson",
+                    Email = "dflorez6@gmail.com",
+                    CourseId = 2,
+                    StatusId = "ConfirmationMessageNotSent"
+                },
+                new Student
+                {
+                    StudentId = 4,
+                    Name = "Homer Simpson",
+                    Email = "dflorez6.dev@gmail.com",
+                    CourseId = 2,
+                    StatusId = "ConfirmationMessageNotSent"
+                },
+                new Student
+                {
+                    StudentId = 5,
+                    Name = "Maggie Simpson",
+                    Email = "dflorez6@gmail.com",
+                    CourseId = 3,
+                    StatusId = "ConfirmationMessageNotSent"
+                },
+                new Student
+                {
+                    StudentId = 6,
+                    Name = "Ned Flanders",
+                    Email = "dflorez6.dev@gmail.com",
+                    CourseId = 3,
+                    StatusId = "ConfirmationMessageNotSent"
+                }
+            );
+
         }
 
     }
